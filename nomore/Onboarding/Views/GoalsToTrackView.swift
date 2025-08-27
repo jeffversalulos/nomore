7fa9036a1718ec4775e9c61ed2ed12b406f96c1f
jeffversalulos <jeffversalulos@gmail.com>
2025-08-27 17:07:26 -0400

    new commit

---
+++ nomore/Onboarding/Views/GoalsToTrackView.swift
@@ -0,0 +1,18 @@
+//
+//  GoalsToTrackView.swift
+//  nomore
+//
+//  Created by Aa on 2025-08-27.
+//
+
+import SwiftUI
+
+struct GoalsToTrackView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GoalsToTrackView()
+}
